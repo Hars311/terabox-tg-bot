faa53159e3089d41e721ec745a1de7db74c77a5a
Harsh <159182118+Hars311@users.noreply.github.com>
2026-01-05 16:17:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 BOT_TOKEN = "8148103361:AAG5yQeRhp5oIbpo8mDVqcoY4c6LVmAho40"
-TERABOX_COOKIE = "browserid=rkfhGXQURT8dKo9mw8KV2WhQUFHtZF4Dsww0OkOH_8vXHQr_12sTs0EM9kE=; lang=en; TSID=6GjvE4DXGJynFsNsxU6pxeiJjvy5rty9; __bid_n=19b8d74d542600bbdc4207; ndus=YyVskB9peHui-OkBnqSF5YzZuReuuw2MFRKQ5_vZ; csrfToken=eRRjJyhAYIjiKK44nNP0ZX7f; __stripe_mid=6be9a5eb-f915-4e67-882a-5ba16969e4561f9f60; __stripe_sid=16bc9bde-123b-49ec-90e9-be3619da8026c3b496; ndut_fmv=6b29a69122915448cefa4651753dae56362a00225dcfdf4a2144cf0e72d35ac5028b940438655979f423c8968886b5487b9e64d41d1c9f75bafbda6a81e4dc386dbcf7d1af60c920aa1833832d5d4eafa9e6824ea3b6a9cab0742900bae4c2d18f9ad65cb4b4e20a4eb186a6ee513295; ndut_fmt=9C04E47A8C33DCF3CF8052D90C04A5A265A37858BB642858306301BDF4D47DAE; g_state={"i_l":0,"i_ll":1767608852245,"i_b":"zbTh5ikhDjTPFpeWlEnCNuVIi6v1g2pvWSaVcBylKjc","i_e":{"enable_itp_optimization":1}}"
+TERABOX_COOKIE = "browserid=rkfhGXQURT8dKo9mw8KV2WhQUFHtZF4Dsww0OkOH_8vXHQr_12sTs0EM9kE=; lang=en; TSID=6GjvE4DXGJynFsNsxU6pxeiJjvy5rty9; __bid_n=19b8d74d542600bbdc4207; ndus=YyVskB9peHui-OkBnqSF5YzZuReuuw2MFRKQ5_vZ; csrfToken=eRRjJyhAYIjiKK44nNP0ZX7f; __stripe_mid=6be9a5eb-f915-4e67-882a-5ba16969e4561f9f60; __stripe_sid=16bc9bde-123b-49ec-90e9-be3619da8026c3b496; ndut_fmv=6b29a69122915448cefa4651753dae56362a00225dcfdf4a2144cf0e72d35ac5028b940438655979f423c8968886b5487b9e64d41d1c9f75bafbda6a81e4dc386dbcf7d1af60c920aa1833832d5d4eafa9e6824ea3b6a9cab0742900bae4c2d18f9ad65cb4b4e20a4eb186a6ee513295; ndut_fmt=9C04E47A8C33DCF3CF8052D90C04A5A265A37858BB642858306301BDF4D47DAE"
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Send video/file directly, I'll upload to TeraBox.")
